ec8ffd4a7b0f4d66811ff13ce3eb5f79532c23a5
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-09-27 08:25:04 +0700

    Initial Commit

---
+++ SwiftUI Bootcamp IDN/SwiftUI_Bootcamp_IDNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bootcamp_IDNApp.swift
+//  SwiftUI Bootcamp IDN
+//
+//  Created by Mufti Ramdhani on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bootcamp_IDNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
